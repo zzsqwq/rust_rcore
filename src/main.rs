bc3b5c3b3200b733da2c34ee839a527d1252e491
csy <505007945@qq.com>
2020-11-21 16:23:41 +0800

    添加了向console输出字符的function

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,21 @@ fn panic(_info: &PanicInfo) -> !{
 global_asm!(include_str!("entry.asm"));
 
 use core::panic::PanicInfo;
-
+pub fn console_putchar(ch: u8){
+    let _ret: usize;
+    let arg0: usize = ch as usize;
+    let arg1: usize = 0;
+    let arg2: usize = 0;
+    let which: usize = 1;
+    unsafe{
+        llvm_asm!("ecall"
+             : "={x10}" (_ret)
+             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
+             : "memory"
+             : "volatile"
+        );
+    }
+}
 /// Rust 的入口函数
 ///
 /// 在 `_start` 为我们进行了一系列准备之后，这是第一个被调用的 Rust 函数
