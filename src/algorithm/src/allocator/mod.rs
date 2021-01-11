07f8cfb6f3f8289b523c93991746f867be6bb819
csy <505007945@qq.com>
2021-01-11 21:13:43 +0800

    对实验2代码的分析理解

---
+++ src/algorithm/src/allocator/mod.rs
@@ -0,0 +1,24 @@
+//! 负责分配 / 回收的数据结构
+
+mod stacked_allocator;
+mod segment_tree_allocator;
+
+/// 分配器：固定容量，每次分配 / 回收一个元素
+pub trait Allocator {
+    /// 给定容量，创建分配器
+    fn new(capacity: usize) -> Self;
+    /// 分配一个元素，无法分配则返回 `None`
+    fn alloc(&mut self) -> Option<usize>;
+    /// 回收一个元素
+    fn dealloc(&mut self, index: usize);
+}
+
+
+pub use stacked_allocator::StackedAllocator;
+pub use segment_tree_allocator::SegmentTreeAllocator;
+
+/// 默认使用的分配器
+pub type AllocatorImpl = StackedAllocator;
+
+//pub type AllocatorImpl = SegmentTreeAllocator;
+
